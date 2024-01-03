a2995abf5cc97f69ce9b39b4f9d298babf27232d
abdelrahmanelbasel <154310788+abdelrahmanelbasel@users.noreply.github.com>
2024-01-03 13:21:24 +0000

    Add daila

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,8 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen w-screen overflow-hidden">
       <Background />
-      <h1 className={twMerge("text-8xl font-extrabold text-blue-400", notoClassName)}>Elbasel</h1>
+      <h1 className={twMerge("text-8xl font-extrabold text-blue-400", notoClassName)}>Thank You Ya Dalia</h1>
+      <h2>Get Better Soon!</h2>
     </main>
   );
 }
